15d00cf04a76c3e6c04538bf296466f659bf4c80
Salamander154 <shackle-bobtail.5o@icloud.com>
2025-03-11 18:55:58 -0700

    updates yay

--- Lesson3/MenuProd/Menu.cpp
+++ Lesson3/MenuProd/Menu.cpp
@@ -37,22 +37,122 @@ void coinFlip()
     }
 }
 
+void diceRoll()
+{
+    srand(static_cast<unsigned int>(time(nullptr)));
+    int choice;
+    int min = 2;
+    int max = 1;
+    while (min >= max)
+    {
+        cout << "Please pick the minimum number. \n";
+        cin >> choice;
+        min = choice;
+        cout << "Please pick the maximum number. \n";
+        cin >> choice;
+        max = choice;
+        if (min > max)
+        {
+            cout << "Invalid min and max. \n";
+        }
+        else if (min < max)
+        {
+            int number = min + rand() % (max - min + 1);
+            cout << "You got " << number << "\n";
+        }
+    }
+}
+
+void rockPS()
+{
+    int choice = 5;
+    int min = 1;
+    int max = 3;
+    while (choice >= 4 || choice <= 0)
+    {
+        cout << "What do you pick? \n";
+        cout << "1.Rock 2.Paper 3.Scissors \n";
+        cin >> choice;
+        switch (choice)
+        {
+        case 1:
+        {
+            cout << "You pick rock. \n";
+            break;
+        }
+        case 2:
+        {
+            cout << "You pick paper. \n";
+            break;
+        }
+        case 3:
+        {
+            cout << "You pick scissors. \n";
+            break;
+        }
+        default:
+        {
+            cout << "Invalid option. \n";
+        }
+        }
+    }
+    int number = min + rand() % (max - min + 1);
+    if (number == choice)
+    {
+        cout << "You tie. \n";
+    }
+    else if ((number == 1 && choice == 2) || (number == 2 && choice == 3) || (number == 3 && choice == 1))
+    {
+        cout << "You WIN! \n";
+    }
+    else
+    {
+        cout << "You LOOSE. \n";
+    }
+
+}
+
 int main()
 {
     int choice;
-    cout << "What would you like to do. \n";
-    cout << "1.Number guesser game 2.Coin Flip \n";
-    cin >> choice;
-    switch (choice)
+    do
     {
-    case 1:
-    {
-        guessNum();
-    }
-    case 2:
-    {
-        coinFlip();
-    }
-    }
+        cout << "What would you like to do. \n";
+        cout << "0.Exit 1.Number guesser game 2.Coin Flip 3.Dice 4.Rock, Paper, Scissors \n";
+        cin >> choice;
+        switch (choice)
+        {
+        case 0:
+        {
+            cout << "Ok bye! \n";
+            return 0;
+        }
+        case 1:
+        {
+            guessNum();
+            break;
+        }
+        case 2:
+        {
+            coinFlip();
+            break;
+        }
+        case 3:
+        {
+            diceRoll();
+            break;
+        }
+        case 4:
+        {
+            rockPS();
+            break;
+        }
+        default:
+        {
+            cout << "Invalid answer. \n";
+            break;
+        }
+        }
+    } while (choice != 0);
 
 }
