8413bf7977886741d0b113083c9927b159f3b6b7
Salamander154 <shackle-bobtail.5o@icloud.com>
2025-07-29 20:00:08 -0700

    aaaaa

--- Lesson 10/BinSort/BinSorter.cpp
+++ Lesson 10/BinSort/BinSorter.cpp
@@ -18,6 +18,20 @@ int binarySearch(const vector<int>& arr, int value) {
 
     return -1;
 }
+int secondBinarySearch(const vector<int>& arr, int value)
+{
+    return binarySearchHelper(arr, 0, arr.size() - 1, value);
+}
+int binarySearchHelper(const vector<int>& arr, int left, int right, int value){
+    int mid = left + (right - left) / 2;
+
+    if (arr[mid] == value)      return mid;
+    else if (arr[mid] < value)  return binarySearchHelper(arr, mid + 1, right, value);
+    else if (left > right)      return -1;
+    else                        return binarySearchHelper(arr, left, mid - 1, value); 
+}
+
+
 
 int main() {
     vector<int> arr = { 1, 3, 5, 7, 9, 11 };
